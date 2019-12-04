b86980e70cb7f0a9c6699a2d462524f060a68949
xzq <1192227411@qq.com>
2019-12-04 17:35:27 +0800

    生成训练集与测试集

---
+++ Detector/test.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# Author: xzq
+# Date: 2019-11-26 18:04
+
+from random import shuffle
+
+arr = ["../Data/I/01.jpg 122 34 256 178 422",
+       "../Data/I/02.jpg 12 44 516 718 22",
+       "../Data/I/03.jpg 16 36 546 78 222",
+       "../Data/I/04.jpg 14 5 56 78 232"]
+shuffle(arr)
+
+print(arr)
